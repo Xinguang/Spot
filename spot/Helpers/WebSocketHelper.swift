2e9ca1c097882a33b963f8e96b15b16936d44024
starboychina <admin@kansea.com>
2015-02-04 15:50:31 +0900

    socketIO

--- spot/Helpers/WebSocketHelper.swift
+++ spot/Helpers/WebSocketHelper.swift
@@ -21,10 +21,23 @@ protocol WebSocketHelperDelegate:WebSocketHelperDelegateObjectiveC{
 }
 
 class WebSocketHelper: NSObject, WebSocketDelegate {
+    /* 開発室
     private var scheme = "ws"
     private var host = "192.168.10.161:9000"
     private var path = "/echobot"
     private var protocols = ["chat", "superchat"]
+    */
+    //AWS
+    private var scheme = "ws"
+    private var host = "54.92.98.113"
+    private var path = "/socket.io/?EIO=2&transport=websocket"
+    private var protocols = ["chat", "superchat"]
+    
+    
+    private var socketEvent = "chat"
+    
+    
+    private var issocketio = false;
     var socket:WebSocket?
     var delegate:WebSocketHelperDelegate?
     
@@ -41,9 +54,11 @@ class WebSocketHelper: NSObject, WebSocketDelegate {
         static var instance: WebSocketHelper?
         static var token: dispatch_once_t = 0
     }
+    
     //初期化
     private override init() {
         super.init()
+        self.issocketio = self.path =~ "socket.io"
         self.socket = WebSocket(url: NSURL(scheme: self.scheme, host: self.host, path: self.path)!, protocols: self.protocols)
         self.socket?.delegate = self
         self.socket?.connect()
@@ -86,7 +101,15 @@ class WebSocketHelper: NSObject, WebSocketDelegate {
     }
     
     func websocketDidReceiveMessage(text: String) {
-        self.delegate?.whenSuccess(text)
+        if self.issocketio {
+            let res = text.match("(?<=42\\[\"\(self.socketEvent)\",)(.*)(?=\\])")//self.socketEvent
+            if(res.count>0){
+                self.delegate?.whenSuccess(res[0])
+            }
+        }else{
+            self.delegate?.whenSuccess(text)
+        }
+        //self.delegate?.whenSuccess(text)
     }
     
     func websocketDidReceiveData(data: NSData) {
@@ -96,13 +119,16 @@ class WebSocketHelper: NSObject, WebSocketDelegate {
     func sendMessage(msg:String){
         if let so = self.socket? {
             if(so.isConnected){
-                self.socket?.writeString(msg)
+                var str = msg;
+                if self.issocketio {
+                    str = self.createMessageForEvent(self.socketEvent, withArgs: [msg], hasBinary: false, withDatas: 0)
+                }
+                self.socket?.writeString(str)
             }else{
                 self.delegate?.whenError("websocket is not connected");
             }
         }
     }
-    
     func sendData(data:NSData){
         if let so = self.socket? {
             if(so.isConnected){
@@ -120,5 +146,41 @@ class WebSocketHelper: NSObject, WebSocketDelegate {
                 self.socket?.connect()
             }
         }
+    }
+    
+    //socket io message model
+    private func createMessageForEvent(event:String, withArgs args:[AnyObject],
+        hasBinary:Bool, withDatas datas:Int = 0) -> String {
+            
+            var message:String
+            var jsonSendError:NSError?
+            
+            if !hasBinary {
+                message = "42[\"\(event)\""
+            } else {
+                message = "45\(datas)-[\"\(event)\""
+            }
+            
+            for arg in args {
+                message += ","
+                
+                if arg is NSDictionary || arg is [AnyObject] {
+                    let jsonSend = NSJSONSerialization.dataWithJSONObject(arg,
+                        options: NSJSONWritingOptions(0), error: &jsonSendError)
+                    let jsonString = NSString(data: jsonSend!, encoding: NSUTF8StringEncoding)
+                    
+                    message += jsonString!
+                    continue
+                }
+                
+                if arg is String {
+                    message += "\"\(arg)\""
+                    continue
+                }
+                
+                message += "\(arg)"
+            }
+            
+            return message + "]"
     }
 }
