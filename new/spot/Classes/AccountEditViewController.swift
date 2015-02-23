2c2803942c1047c7fa6371ce03f157ee575f98fc
ZhihuaZhang <zhangzhihua.dev@gmail.com>
2015-02-23 12:39:28 +0900

    1

---
+++ new/spot/Classes/AccountEditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AccountEditViewController.swift
+//  spot
+//
+//  Created by 張志華 on 2015/02/22.
+//  Copyright (c) 2015年 e-business. All rights reserved.
+//
+
+import UIKit
+
+class AccountEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ new/spot/Classes/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  spot
+//
+//  Created by 張志華 on 2015/02/21.
+//  Copyright (c) 2015年 e-business. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
